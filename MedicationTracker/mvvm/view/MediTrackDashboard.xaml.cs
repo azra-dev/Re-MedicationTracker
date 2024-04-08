3d08b7c5b600a0b50873e491c0d67cb964d188c0
rjldg <rjlimdeguzman@gmail.com>
2024-04-09 01:37:35 +0800

    Further git reimplementation

---
+++ MedicationTracker/mvvm/view/MediTrackDashboard.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MedicationTracker.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for MediTrackDashboard.xaml
+    /// </summary>
+    public partial class MediTrackDashboard : Window
+    {
+        public MediTrackDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
