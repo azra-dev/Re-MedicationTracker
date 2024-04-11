7c50b47bb3a8a3b5cb38ece4fa5c2c64fbc849fa
Azrael Anonas <azraelanonas@gmail.com>
2024-04-11 14:12:40 +0800

    Create Schedule Modal Pop-Up

---
+++ MedicationTracker/Controls/scheduleTextBox.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MedicationTracker.Controls
+{
+    /// <summary>
+    /// Interaction logic for scheduleTextBox.xaml
+    /// </summary>
+    public partial class scheduleTextBox : UserControl, INotifyPropertyChanged
+    {
+        public scheduleTextBox()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string placeholder;
+        private int widthSize;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set { placeholder = value; OnPropertyChanged("Placeholder"); }
+        }
+        public int WidthSize
+        {
+            get { return widthSize; }
+            set { widthSize = value; OnPropertyChanged("WidthSize"); }
+        }
+
+
+        // event
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
