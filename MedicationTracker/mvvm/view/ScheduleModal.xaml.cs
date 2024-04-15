c0b5eaae30c4f4e7f2213e8560cedc94a8310b07
GODWYN SUMMER MERCADO <gsjmercado@mymail.mapua.edu.ph>
2024-04-15 15:22:21 +0800

    Added Lagic Circuit

--- MedicationTracker/mvvm/view/ScheduleModal.xaml.cs
+++ MedicationTracker/mvvm/view/ScheduleModal.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,11 +20,34 @@ namespace MedicationTracker.MVVM.View
     /// <summary>
     /// Interaction logic for ScheduleModal.xaml
     /// </summary>
-    public partial class ScheduleModal : Window
+    public partial class ScheduleModal : Window, INotifyPropertyChanged
     {
         public ScheduleModal()
         {
+            DataContext = this;
             InitializeComponent();
+        }
+
+        private string scheduleMode = null;
+        public string ScheduleMode
+        {
+            get { return scheduleMode = null; }
+            set { 
+                scheduleMode = value;
+                Trace.WriteLine(scheduleMode);
+                if      (value == "System.Windows.Controls.ComboBoxItem: Daily")  { ScheduleList_Daily.Visibility = Visibility.Visible;   ScheduleList_Weekly.Visibility = Visibility.Collapsed; }
+                else if (value == "System.Windows.Controls.ComboBoxItem: Weekly") { ScheduleList_Daily.Visibility = Visibility.Collapsed; ScheduleList_Weekly.Visibility = Visibility.Visible  ; }
+                else                                                              { ScheduleList_Daily.Visibility = Visibility.Collapsed; ScheduleList_Weekly.Visibility = Visibility.Collapsed; }
+                OnPropertyChanged("ScheduleMode"); 
+            }
+        }
+
+
+        // events
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
