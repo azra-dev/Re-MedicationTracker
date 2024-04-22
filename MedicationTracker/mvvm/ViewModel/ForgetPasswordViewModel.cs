4e62ab04485f2956800d5caa5e03b89525c82c74
Azrael Anonas <azraelanonas@gmail.com>
2024-04-22 16:40:12 +0800

    Ongoing Issue of Email API

--- MedicationTracker/mvvm/ViewModel/ForgetPasswordViewModel.cs
+++ MedicationTracker/mvvm/ViewModel/ForgetPasswordViewModel.cs
@@ -23,7 +23,7 @@ namespace MedicationTracker.MVVM.ViewModel
 {
     internal class ForgetPasswordViewModel : ObservableObject
     {
-        public RelayCommand SendEmailCommand => new RelayCommand(execute => SendEmail());
+        public RelayCommand SendEmailCommand => new RelayCommand(execute => SendMail());
 
         public ForgetPasswordViewModel()
         {
@@ -37,42 +37,22 @@ namespace MedicationTracker.MVVM.ViewModel
             set { emailCredential = value; OnPropertyChanged("EmailAddressInput");  }
         }
 
-        public void SendEmail()
+        public void SendMail()
         {
-            _SendEmail().Wait();
+            RunAsync();
         }
-
-        static async Task _SendEmail()
+        public void RunAsync()
         {
-            // set up mail content
-            MailjetClient client = new MailjetClient(Environment.GetEnvironmentVariable("473c936e94650da78c723e31ce02bda9"), Environment.GetEnvironmentVariable("7c7cd63a1299381acec1483b3bae1ac2"));
-            MailjetRequest request = new MailjetRequest
-            {
-                Resource = Send.Resource,
-            }
-               .Property(Send.FromEmail, "meditrack@mailjet.com")
-               .Property(Send.FromName, "MediTrack Mail Service")
-               .Property(Send.Subject, "Password Recovery")
-               .Property(Send.TextPart, "You forgot your password because you did not use our MediTrack services and hence, you have not took your memory-allocation medication.\r\n " +
-                                        "Do not want to forget this message? You will forgot you read this email anyway and take a look again after a while ")
-               .Property(Send.HtmlPart, "<html>\r\n             <body>\r\n                 <p><b>You forgot your password</b> because <i>you did not use our MediTrack services</i> and hence, you have not took your memory-allocation medication.\r\n                 Do not want to forget this message? You will forgot you read this email anyway and take a look again after a while :)</p>\r\n             </body>\r\n         </html>")
-               .Property(Send.Recipients, new JArray {
-                new JObject {
-                 {"Email", "aztx.256@gmail.com"}
-                 }
-                   });
-            MailjetResponse response = await client.PostAsync(request);
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
-                Console.WriteLine(response.GetData());
-            }
-            else
-            {
-                Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
-                Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
-                Console.WriteLine(string.Format("ErrorMessage: {0}\n", response.GetErrorMessage()));
-            }
+            string to = "acanonas@mymail.mapua.edu.ph";
+            string from = "acanonas@mymail.mapua.edu.ph";
+            string subject = "MediTrack";
+            string body = "this is a test email. you have forgoten your password due to not taking your memory-allocation medication.";
+            MailMessage message = new MailMessage(from, to, subject, body);
+            SmtpClient smtp = new SmtpClient("in-v3.mailjet.com");
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential("473c936e94650da78c723e31ce02bda9", "7c7cd63a1299381acec1483b3bae1ac2");
+            smtp.EnableSsl = true;
+            smtp.Send(message);
 
 
         }
